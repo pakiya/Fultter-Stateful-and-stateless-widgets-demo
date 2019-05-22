22e0a63d126d06c08f61e02bf9f86e4e4b5738ab
Pankaj kumar <pk.kumar7500@gmail.com>
2019-05-22 12:45:38 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import './ui/make_it_rain.dart';
+
+main() {
+  runApp(new MaterialApp(
+    title: "Make it Rain!",
+    home: new MakeItRain(),
+  ));
+}
